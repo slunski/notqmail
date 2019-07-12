ddaacf11d76db657d7a05ba572fc7c3f6e56b0b3
Rolf Eike Beer <eike@sf-mail.de>
2019-07-12 19:12:14 +0200

    get rid of byte_zero() and use the default memset() instead

--- byte.h
+++ byte.h
@@ -1,12 +1,14 @@
 #ifndef BYTE_H
 #define BYTE_H
 
+#include <string.h>
+
 extern unsigned int byte_chr();
 extern unsigned int byte_rchr();
 extern void byte_copy();
 extern void byte_copyr();
 extern int byte_diff();
-extern void byte_zero();
+#define byte_zero(s,n) memset(s,0,n)
 
 #define byte_equal(s,n,t) (!byte_diff((s),(n),(t)))
 
